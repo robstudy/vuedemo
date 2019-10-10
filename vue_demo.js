c6016a1be70302db98bfa5858b818f0b666776cb
robstudy <robgarza@mail.fresnostate.edu>
2019-10-10 12:06:09 -0700

    moved template into index.html, now able to switch between blog post and all blogs display by clicking link on post

--- vue_demo.js
+++ vue_demo.js
@@ -2,7 +2,9 @@ var app = new Vue({
 	el: '#app',
 	data: {
 		base_url: 'https://thefifthmoon.herokuapp.com',
-		blogs: []
+		blogs: [],
+		loaded_blog: {},
+		display_post: false
 	},
 	mounted() {
 		axios
@@ -13,36 +15,11 @@ var app = new Vue({
 				});
 			});
 	},
-	template: `
-		<div class="container">
-			<div v-for="blog in blogs">
-				<div class="row">
-					<div class="col-10">
-						<div class="card">
-						<div class="card-body">
-						<div class="container">
-						<div class="row">
-						<div class="col-2">
-							<a v-bind:href="base_url + '/blog/' + blog.slug_name">
-							<img v-bind:src="base_url + '/static/images/' + getImage(blog.tags)" class="img-thumbnail img-fluid rounded float-left" style="margin-right: 1em;" alt="topic image"></a>
-						</div><!--/col-2-->
-						<div class="col-10">
-							<h5 class="card-title">
-							<a v-bind:href="base_url + '/blog/' + blog.slug_name">{{ blog.title }}</a> 
-							{{ prettyDate(blog.pub_date) }}</h5> 
-							<p class="card-text"><span v-html="blog.body.slice(0,100)"></span> .....</p>	
-						</div><!--/col-10-->
-						</div><!--/row-->
-						</div><!--/container-->
-						</div><!--/card-body-->
-						</div><!--/card-->			
-					</div>
-					<div class="col-2"></div>
-				</div>
-			</div>
-		</div>
-	`,
 	methods: {
+		load_blog: function(index) {
+			this.loaded_blog = this.blogs[index];
+			this.display_post = true;
+		},
 		prettyDate: function(time_string) {
 			return this.getMonth(time_string.slice(5,7)) + ' ' +
 					time_string.slice(8,10) + ', ' +
