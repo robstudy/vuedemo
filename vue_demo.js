4cdfe55275b182839e31f7e8b0c9fe36b63f5863
robstudy <robgarza@mail.fresnostate.edu>
2019-11-05 20:49:50 -0800

    changed axios to take blog information from new API link on blog. Changed variables accordingly, removed unused functions

--- vue_demo.js
+++ vue_demo.js
@@ -12,10 +12,10 @@ var app = new Vue({
 	},
 	mounted() {
 		axios
-			.get(this.base_url + '/api/allblogs/')
+			.get(this.base_url + '/api/v1/blogs/')
 			.then(response => {
 				response.data.forEach(function(item,index){
-					app.blogs.push(item.fields);
+					app.blogs.push(item);
 				});
 			});
 
@@ -27,51 +27,6 @@ var app = new Vue({
 			this.filtered_blogs = this.blogs;
 			this.inputs.text = '';
 			this.display_post = true;
-		},
-		prettyDate: function(time_string) {
-			return this.getMonth(time_string.slice(5,7)) + ' ' +
-					time_string.slice(8,10) + ', ' +
-					time_string.slice(0,4);
-		},
-		getMonth: function(month_string) {
-			switch(month_string) {
-				case '01': return 'Jan';
-				case '02': return 'Feb';
-				case '03': return 'Mar';
-				case '04': return 'Apr';
-				case '05': return 'May';
-				case '06': return 'Jun';
-				case '07': return 'Jul';
-				case '08': return 'Aug';
-				case '09': return 'Sep';
-				case '10': return 'Oct';
-				case '11': return 'Nov';
-				case '12': return 'Dec';
-				default: return 'not a valid month';
-			}
-		},
-		getImage: function(tags) {
-			if (tags.includes('software engineering') || tags.includes('algorithms')) {
-				return 'cs_bg.jpg';
-			}
-			else if(tags.includes('cpp')){
-				return 'cpp_logo.png';			
-			}
-			else if(tags.includes('python')){
-				return 'python.png';			
-			}
-			else if(tags.includes('javascript')){
-				return 'js.png';			
-			}
-			else if(tags.includes('book') || tags.includes('books')){
-				return 'book.png';			
-			}
-			else if(tags.includes('cpp')){
-				return 'cpp_logo.png';			
-			} 
-			else {
-				return 'random.png';
-			}
 		},
 		search: function() {
 			this.filtered_blogs = [];
